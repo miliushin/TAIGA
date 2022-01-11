4883e7c19dabc6f5e0a0d3a4d49c2ba292d3c7e9
miliushin <naurdinm@gmail.com>
2022-01-11 20:08:22 +0800

    change muon.h

--- programs/TMuon/include/muon.h
+++ programs/TMuon/include/muon.h
@@ -17,12 +17,10 @@ class MUON {
 
   double GetTime(unsigned char ch[8]);
   int ReadADC(FILE *data_file);
-  int GetAmplitude(int ch, double d[Aperture]);
 
   unsigned int ID;                              // Station indentification
   unsigned int NumBytes;                        // Data packet size
-  unsigned int NumSt;
-  int data[NUMBER_OF_CHANEL][Aperture];
+  int data[NUMBER_OF_CHANEL][Aperture];         // ADC data
   unsigned int fin;
 
  private:
@@ -34,10 +32,6 @@ class MUON {
   unsigned long int i_File;
 
   Int_t NumEvent;  // Number of event
-
-  TGraph *GData;
-  TGraph *GDataNew;
-  TCanvas *can;
   TTree *TData;
 };
 
