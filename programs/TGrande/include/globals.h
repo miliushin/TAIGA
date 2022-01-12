297d7451aa1d16397fa1b56fd4e3bb644575315f
miliushin <naurdinm@gmail.com>
2022-01-13 03:34:43 +0800

    Update globals.h

--- programs/TGrande/include/globals.h
+++ programs/TGrande/include/globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 
 #define SIZE_OF_HEADER            24
-#define Aperture  			      1024
+#define Aperture                  1024
 #define NUMBER_OF_CHANNELS        12
 #define NUMBER_OF_STATIONS        19
 #define MAX_OF_DAYS               100
